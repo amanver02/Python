54e317972be009f5f70d28a185368cec792922c0
amanver02 <developeraman02@gmail.com>
2025-03-01 10:36:41 +0530

    chapter4

--- Python Chapter 3/02_str_functions.py
+++ Python Chapter 3/02_str_functions.py
@@ -5,6 +5,3 @@ print(name.startswith("am"))
 print(name.capitalize())
 print(name.upper())
 print(name.lower())
-print(name.replace("aman", "verma"))
-print(name)
-print("hello world".replace("world", "Python"))  # Output: "hello Python"
