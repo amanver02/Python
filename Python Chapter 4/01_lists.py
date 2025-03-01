ea26c09e65314bfa2350a546b89af571d5d3741f
amanver02 <developeraman02@gmail.com>
2025-03-01 10:47:44 +0530

    chapter 4

---
+++ Python Chapter 4/01_lists.py
@@ -0,0 +1,9 @@
+friends  = ["aman" , "orange", False , 5,"verma"]
+print(friends[2 : 4])
+
+# l1.sort(): updates the list to [1,2,7,8,15,21]
+# • l1.reverse(): updates the list to [15,21,2,7,8,1]
+# • l1.append(8): adds 8 at the end of the list
+# • l1.insert(3,8): This will add 8 at 3 index
+# • l1.pop(2): Will delete element at index 2 and return its value.
+# • l1.remove(21): Will remove 21 from the list. 
